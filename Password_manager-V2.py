fdb1fa9922823fc23fc1e4e58cfd9dd678cef3fd
elmeddinnazarov <elmeddin.nazarov0@gmail.com>
2022-11-06 16:26:24 +0400

    same functions added

--- Password_manager-V2.py
+++ Password_manager-V2.py
@@ -41,6 +41,8 @@ def validate_pswd(pswd):
     if len(pswd) < 8:
         print("at least 8 character please!")
         return False
+    elif pswd.isnumeric():
+        print("The password must include least 2 letter, 1 uppercase and 1 lowercase.")
     elif pswd.isupper() or pswd.islower():
         print("The password must include least 1 uppercase and 1 lowercase.")
         return False
@@ -85,14 +87,15 @@ def wrong_attempt():
     elif sec == "3":
         sign_up()
     else:
-        print("bye bye")
+        print("See you again!")
 
 def restore_pswd():
     email_correct = False
     key_correct = False
     name_correct = False
     numb_correct = False
-    while True:
+    status = True
+    while status:
         if not email_correct:
             mail = input("Enter your mail address: ").lower()
             if validate_mail(mail):
@@ -116,17 +119,19 @@ def restore_pswd():
             1- Turkey (+90)
             2- Azerbaijan (+994)
             """
-            while True:
+            status_phone = True
+            while status_phone:
                 print(text_2)
                 select = input(": ")
                 if select == "1":
                     code = "+90"
-                    break
+                    status_phone = False
                 elif select == "2":
                     code = "+994"
-                    break
+                    status_phone = False
                 else:
                     print("Plese select country code correctly!")
+                    status_phone = True
 
             number = input(
                 f"Enter your phone number without leading '0': {code} ")
@@ -154,7 +159,8 @@ def restore_pswd():
 
             except FileNotFoundError:
                 wrong_attempt()
-            
+                status = False
+            login_attempt = False
             for user in users:
                 if mail_hash == user["mail"] and key_hash == user["key"] and nmbr_hash == user["number"] and first_name == user["first name"] and last_name == user["last name"]:
                     print("Verification Complated! \n\n")
@@ -181,8 +187,55 @@ def restore_pswd():
                                     json.dump(big_dict, r)
                                 input("Password successfully changed! Click 'Enter' to Main Menu: ")
                                 intro()
-                    else:
-                        wrong_attempt()
+                    login_attempt = True
+            if login_attempt == False:
+                wrong_attempt()
+                status = False
+
+
+def encyrption(input_value):
+    import random
+    import string
+    import textwrap
+
+    # global result
+    output = textwrap.wrap(input_value, 1)
+    len_output = len(output)
+    len_len_output = len(str(len_output))
+    index = int(119/len_output)
+    aralık = index
+    len_aralık = len(str(aralık))
+    letters = string.ascii_letters + string.digits +string.digits +string.punctuation
+    random_str = ''.join(random.choice(letters) for i in range(120))
+    for newstring in output: 
+        random_str = random_str[:index] + newstring + random_str[index + 1:]
+        index += aralık
+    random_str = str(len_output).rjust(2,"&") + str(random_str)[10] + str(len_len_output) + str(random_str) + str(aralık).rjust(2,"%") + str(random_str)[10] + str(len_aralık)
+    result = random_str
+    return result
+
+def decyrption(result):
+    # global user_pw
+    password = []
+    first_part = result[:4]
+    if not "&" in first_part:
+        pw_lenght = first_part[:2]
+    else:
+        pw_lenght = first_part[1]
+    pw_lenght = int(pw_lenght)
+    second_part = result[-4:]
+    if not "%" in second_part:
+        pw_range = second_part[:2]
+    else:
+        pw_range = second_part[1]
+    pw_range = int(pw_range)
+    str_part = result[4:-4]
+    st_range = pw_range
+    for pw in range(pw_lenght):
+        password.append(str_part[pw_range])
+        pw_range+=st_range
+    user_pw = "".join(password)
+    return user_pw
 
 def sign_up():
     email_correct = False
@@ -190,7 +243,8 @@ def sign_up():
     key_correct = False
     name_correct = False
     numb_correct = False
-    while True:
+    status_signup = True
+    while status_signup:
         if not email_correct:
             mail = input("Enter your mail address: ").lower()
             if validate_mail(mail):
@@ -218,15 +272,16 @@ def sign_up():
             1- Turkey (+90)
             2- Azerbaijan (+994)
             """
-            while True:
+            status_phone = True
+            while status_phone:
                 print(text_2)
                 select = input(": ")
                 if select == "1":
                     code = "+90"
-                    break
+                    status_phone = False
                 elif select == "2":
                     code = "+994"
-                    break
+                    status_phone = False
                 else:
                     print("Plese select country code correctly!")
 
@@ -282,12 +337,13 @@ def sign_up():
             intt = input("Enter to 'Y' go to main menu, 'Q' to exit...")
             if intt.lower() == "y":
                 intro()
+                status_signup = False
             else:
-                break
-            break
+                status_signup = False
 
 def sign_in():
-    while True:
+    status_signin = True
+    while status_signin:
         mail_int = input("Your mail: ")
         pswd_int = input("Your password: ")
         if validate_mail(mail_int) and validate_pswd(pswd_int):
@@ -304,14 +360,19 @@ def sign_in():
                     users = big_dict["users"]
             except FileNotFoundError:
                 wrong_attempt()
-
+            login_attempt = False
             for user in users:
                 if login_mail == user["mail"] and login_pswd == user["password"]:
                     current_user = user
                     signed_in(current_user, big_dict)
+                    login_attempt = True
                     break
-                else:
-                    wrong_attempt()
+            if login_attempt == False:
+                wrong_attempt()
+                status_signin = False
+
+
+
 
 def signed_in(current_user, big_dict):
     sign_int = input("""
@@ -329,17 +390,17 @@ def signed_in(current_user, big_dict):
     if sign_int == "1":
         user_platforms(current_user, big_dict)
     elif sign_int == "2":
-        pl_pswd_change(current_user)
+        account_pswd_change(current_user)
     elif sign_int == "3":
-        pl_mail_change(current_user)
+        account_mail_change(current_user)
     elif sign_int == "4":
-        pl_key_change(current_user)
+        account_key_change(current_user)
     elif sign_int == "5":
         intro()
     else:
         pass
 
-def pl_pswd_change(current_user):
+def account_pswd_change(current_user):
             try:
                 with open("passwords.json") as r:
                     big_dict=json.load(r)
@@ -373,7 +434,7 @@ def pl_pswd_change(current_user):
                     else:
                         wrong_attempt()
 
-def pl_mail_change(current_user):
+def account_mail_change(current_user):
     try:
         with open("passwords.json") as r:
             big_dict=json.load(r)
@@ -407,7 +468,7 @@ def pl_mail_change(current_user):
             else:
                 wrong_attempt()
 
-def pl_key_change(current_user):
+def account_key_change(current_user):
     try:
         with open("passwords.json") as r:
             big_dict=json.load(r)
@@ -455,28 +516,27 @@ def user_platforms(current_user, big_dict):
     : """)
 
     if pl_int == "1":
-        platforms_info(current_user)
+        show_platforms(current_user, big_dict)
     elif pl_int == "2":
         add_platform(current_user, big_dict)
     elif pl_int == "3":
-        change_pl_info()
+        change_pl_info(current_user, big_dict)
     elif pl_int == "4":
-        delete_platform()
+        delete_platform(current_user, big_dict)
     elif pl_int == "5":
         intro()
     elif pl_int == "6":
         signed_in(current_user)
     else:
-        pass
+        "q a basılanda sign in e gedir. ne yazılmalıdır ki getmesin "
 
-def platforms_info(current_user):
+def show_platforms(current_user, big_dict):
         pl_info = input("""
     
         1- List All Platforms,
         2- List by Platform name,
-        3- List by Username,
-        4- Log out,
-        5- Back Menu,
+        3- Log out,
+        4- Back Menu,
         q- Exit
 
         : """)
@@ -484,13 +544,11 @@ def platforms_info(current_user):
         if pl_info == "1":
             all_platforms(current_user)
         elif pl_info == "2":
-            add_platform(big_dict)
+            list_by_name(current_user)
         elif pl_info == "3":
-            change_pl_info()
+            intro()
         elif pl_info == "4":
-            delete_platform()
-        elif pl_info == "5":
-            signed_in(current_user)
+            user_platforms()
         else:
             pass
 
@@ -503,13 +561,23 @@ def all_platforms(current_user):
         try:
             platforms = current_user["platforms"]
             pl_exsist = True
+
+            for platform in platforms:
+                print("""
+                Platform Name: {}
+                Mail Address: {}
+                Username: {}
+                Password: {}
+                """.format(platform["pl_site"], decyrption(platform["pl_mail"]), decyrption(platform["pl_username"]), decyrption(platform["pl_pswd"])))
+
         except KeyError:
-            print("You have not added platform before! Add the platform before 'List Platforms' please!")
-            add_platform(current_user, big_dict)
-            pl_exsist = False
+            pl_input = input("You did not add platform before! To add platform enter 'Y', 'Q' to exit!")
+            if pl_input.isalpha and pl_input.lower() == "y":
+                add_platform(current_user, big_dict)
+                pl_exsist = False
 
         if pl_exsist:
-            pass # platformları güzembir formatta listeleyeceksın
+            pass # platformları güzel bir formatta listeleyeceksın
                 
 def add_platform(current_user, big_dict):
     print("\nPlease enter the platform information you want to add!\n")
@@ -517,6 +585,10 @@ def add_platform(current_user, big_dict):
     pl_username = input("Username: ")
     pl_mail = input("Mail Address: ")
     pl_pswd = input("Password: ")
+
+    pl_username = encyrption(pl_username)
+    pl_mail = encyrption(pl_mail)
+    pl_pswd = encyrption(pl_pswd)
 
     try:
         platforms = current_user["platforms"]
@@ -533,7 +605,37 @@ def add_platform(current_user, big_dict):
     
     with open("passwords.json", "w") as r:
         json.dump(big_dict, r)
+    
+    print("\nPlatform added successfully.\n")
+    user_platforms(current_user, big_dict)
 
+# def list_by_name(current_user):
+#     pl_name = input("Write Platform Name to show information: ")
+#     with open("passwords.json") as r:
+#         big_dict = json.load(r)
+#         users = big_dict["users"]
+#         current_user
+
+#         try:
+#             platforms = current_user["platforms"]
+#             print(platform)
+
+# Hele bu bölüm hazır deyilş hazır olanda push eliyecem xeber eliyecem sene
+
+
+#             for platform in platforms:
+#                 if platform == pl_name:
+#                     print("""
+#                     Platform Name: {}
+#                     Mail Address: {}
+#                     Username: {}
+#                     Password: {}
+#                     """.format(platform["pl_site"], decyrption(platform["pl_mail"]), decyrption(platform["pl_username"]), decyrption(platform["pl_pswd"])))
+
+        except KeyError:
+            pl_input = input("You did not add platform before! To add platform enter 'Y', 'Q' to exit!")
+            if pl_input.isalpha and pl_input.lower() == "y":
+                add_platform(current_user, big_dict)
 
 
 intro()
