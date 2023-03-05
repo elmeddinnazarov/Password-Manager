019e796da01c98714ae1a122f8c9204f46d15b31
Aslan-999 <aslanqehramanovofficial@gmail.com>
2023-03-06 01:51:13 +0400

    Validation functions updated

--- validate.py
+++ validate.py
@@ -1,39 +1,49 @@
+import re
+
+
+# PATTERNS
+
+email_pattern = re.compile("^[a-z0-9]+([._%+-][a-z0-9]+)?@[a-z0-9.]+\.[a-z]{2,}$")
+
+
+
+# VALIDATE FUNCTIONS
+
 def validate_mail(mail):
-    if not "@" in mail[-14:-7] or not "." in mail[-6:-1]:
-        # pattern = r'^[a-zA-Z0-9_][a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if not email_pattern.search(mail):
         print("Mail format not accepted!")
         return False
     else:
         return True
 
 def validate_pswd(pswd):
-    if len(pswd) < 8:
-        print("at least 8 character please!")
-        return False
-    elif pswd.isnumeric():
-        print("The password must include least 2 letter, 1 uppercase and 1 lowercase.")
-    elif pswd.isupper() or pswd.islower():
-        print("The password must include least 1 uppercase and 1 lowercase.")
+    if len(pswd) < 8 or not re.search(r"[A-z]+", pswd) or not re.search(r"\d", pswd) or pswd.islower() or pswd.isupper():
+        print(
+            "At least 8 character please!\n"
+            "Password must contain at least 1 letter or 1 number\n"  
+            "The password must include least 1 uppercase and 1 lowercase.\n"
+            )
         return False
     else:
         return True
-
+    
 def validate_num(number, select):
     if select == "1":
         lenght = 10
     elif select == "2":
         lenght = 9
-    if not len(number) == lenght:
-        print("Please enter your phone number carefully!")
-        return False
-    elif not number.isnumeric():
-        print("Numbers can not contain letters! Please enter your phone number carefully!")
+    if not number.isnumeric() or not len(number) == lenght:
+        print(
+            "Please enter your phone number carefully!\n"
+            "Numbers can not contain letters! Please enter your phone number carefully!"
+              )
         return False
     else:
         return True
 
+
 def validate_key(key):
-    if not 3 < len(key) < 15 and ' ' in key:
+    if not 3 < len(key) < 15 or ' ' in key:
         print("Security word lenght has to be min 3, max 15 characters! And spaces not acceptable!")
         return False
     else:
